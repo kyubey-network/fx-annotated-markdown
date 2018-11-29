f6dda739fc237a901e61ce0ad38754f1269a7598
1@1234.sh <1@1234.sh>
2018-11-29 15:58:16 +0800

    init

---
+++ Andoromeda.Framework.AnnotatedMarkdown/AnnotatedMarkdownParseResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Andoromeda.Framework.AnnotatedMarkdown
+{
+    public class AnnotatedMarkdownParseResult
+    {
+        public string PlainMarkdown { get; set; }
+
+        public IDictionary<string, string> Annotations { get; set; }
+    }
+}
---
+++ Andoromeda.Framework.AnnotatedMarkdown/AnnotationParser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Andoromeda.Framework.AnnotatedMarkdown
+{
+    public static class AnnotationParser
+    {
+        public static AnnotatedMarkdownParseResult Parse(string src)
+        {
+            var splited = src.Split('\n')
+                             .Select(x => x.TrimEnd('\r'));
+            var annotationLines = splited.Where(x => x.StartsWith("@ ") && x.Contains(":"));
+            var dic = new Dictionary<string, string>();
+            foreach(var x in annotationLines)
+            {
+                try
+                {
+                    var colonPosition = x.IndexOf(':');
+                    var key = x.Substring(0, colonPosition);
+                    var value = colonPosition + 1 < x.Length ? x.Substring(colonPosition + 1) : null;
+                }
+                catch { }
+            }
+
+            var plain = splited.Where(x => !x.StartsWith("@ ")).ToList();
+            var emptyLineCount = 0;
+            for (var i = 0; i < plain.Count; i++)
+            {
+                if (string.IsNullOrWhiteSpace(plain[i]))
+                {
+                    ++emptyLineCount;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            var markdown = string.Join("\r\n", plain.Skip(emptyLineCount).ToArray());
+            return new AnnotatedMarkdownParseResult
+            {
+                Annotations = dic,
+                PlainMarkdown = markdown
+            };
+        }
+    }
+}
